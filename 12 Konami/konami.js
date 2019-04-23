ec60abc8b5632867102040775f79cf429b8a715e
KaustubhD <kaustubhd287@gmail.com>
2019-04-23 23:03:52 +0530

    Testing 12 after CORB changes

--- 12 Konami/konami.js
+++ 12 Konami/konami.js
@@ -1,29 +1,27 @@
 let seq = []
 handleKey = ev => {
-  console.log(ev.key)
+  //console.log(ev.key)
   seq.push(ev.key.slice(5))
-  //console.log(seq)
-  //seq.shift()
   seq = seq.slice(-6)
-  console.log(seq)
+  //console.log(seq)
   if(seq.join('') == 'UpDownUpUpDownRight'){
-    console.log('%cDingDingDing !', 'font-size: 30px;font-family: Helvetica')
+    //console.log('%cDingDingDing !', 'font-size: 30px;font-family: Helvetica')
     showGIF()
   }
 }
 
 showGIF = () => {
-  let url = 'http://api.giphy.com/v1/gifs/random?api_key=YGD5bJjlP0OYshZLKMj7tp5YXbaD81mo&q=funny&limit=1'
+  let url = 'https://api.giphy.com/v1/gifs/random?api_key=YGD5bJjlP0OYshZLKMj7tp5YXbaD81mo&tag=funny&limit=1'
   fetch(url, {
-    //mode: 'no-cors',
-    'Access-Control-Allow-Origin':'*',    
+    headers:{
+      'Content-Type': 'application/json', 
+    }
   }).then(raw => raw.json()).then(body => {
-    console.log(body.data)
     let img = document.querySelector('img')
-    img.src = body.data.url
+    img.src = body.data.image_url
     img.style.display = 'block'
   })
 }
 
 document.addEventListener('keydown', handleKey)
-
+console.log('Hint, It\'s: Up Down Up Up Down Right')
