23ef6987ae582ed84e6c2bde06351e9617046483
KaustubhD <kaustubhd287@gmail.com>
2019-05-28 00:44:52 +0530

    19 done

--- 19 Webcam fun/scripts.js
+++ 19 Webcam fun/scripts.js
@@ -20,13 +20,23 @@ let getMedia = () => {
   })
 }
 
-let x
+let x, w, h, px
 let drawToCanvas = () => {
-  canvas.width = video.videoWidth
-  canvas.height = video.videoHeight
+  w = video.videoWidth
+  h = video.videoHeight
+
+  canvas.width = w
+  canvas.height = h
+
 
   x = setInterval(() => {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(video, 0, 0, w, h)
+
+    px = ctx.getImageData(0, 0, w, h)
+    px = redFilter(px)
+    //px = rgbSplit(px)
+
+    ctx.putImageData(px, 0, 0)
   }, 16)
 }
 
@@ -41,6 +51,49 @@ let takeAPhoto = () => {
   link.innerHTML = `<img src="${data}" alt="Screenshot from webcam"/>`
   strip.insertBefore(link, strip.firstChild)
 }
+
+
+let redFilter = (px) => {
+  for(let i = 0; i < px.data.length; i += 4){
+    px.data[i] += 150
+    px.data[i + 1] += 80
+    px.data[i + 2] -= 100
+  }
+  return px
+}
+
+
+
+let rgbSplit = (px) => {
+  for(let i = 0; i < px.data.length; i += 4){
+    px.data[i - 100] = px.data[i]
+    px.data[i + 200] = px.data[i + 1]
+    px.data[i + 300] = px.data[i + 2]
+  }
+  return px
+}
+
+
+let greenScreen = (px) => {
+  const levels = {}
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value
+  })
+  let r,g,b
+  for(let i = 0; i < px.data.length; i += 4){
+    r = px.data[i]
+    g = px.data[i + 1]
+    b = px.data[i + 3]
+
+    if(r >= levels.rmin && r <= levels.rmax &&
+       g >= levels.gmin && g <= levels.gmax &&
+       b >= levels.bmin && b <= levels.bmax)
+       px.data[i + 3] = 0
+  }
+  return px
+}
+
+
 
 getMedia()
 
